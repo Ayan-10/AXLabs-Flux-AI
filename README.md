c17da173ac3f3bccc8a8e67f903efbe5e79b17d2
Ayan Sarkar <71203077+Ayan-10@users.noreply.github.com>
2024-09-29 18:52:47 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<h1 align="center">AXLabs Flux AI</h1>
---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
+import { Navbar } from "@/components/Navbar";
+import TanStackProvider from "@/components/providers/TanStackProvider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "AXLabs Flux AI",
+	description: "Write prompt to generate images",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang='en'>
+			<body className={inter.className}>
+				<ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
+					<TanStackProvider>
+						<Navbar />
+						{children}
+					</TanStackProvider>
+				</ThemeProvider>
+			</body>
+		</html>
+	);
+}
