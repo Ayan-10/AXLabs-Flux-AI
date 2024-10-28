c0dccb5e8153d60f9843abeaada998fcf67b2e75
Ayan Sarkar <71203077+Ayan-10@users.noreply.github.com>
2024-10-28 15:33:48 +0530

    Update page.tsx

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -7,39 +7,33 @@ import { checkAuthStatus } from "./actions";
 import { useEffect } from "react";
 
 const Page = () => {
-  const router = useRouter();
-  const { user } = useKindeBrowserClient();
-  const { data, isLoading } = useQuery({
-    queryKey: ["checkAuthStatus"],
-    queryFn: async () => await checkAuthStatus(),
-    staleTime: Infinity, // Optional: prevent frequent refetches
-  });
+	const router = useRouter();
+	const { user } = useKindeBrowserClient();
+	const { data } = useQuery({
+		queryKey: ["checkAuthStatus"],
+		queryFn: async () => await checkAuthStatus(),
+	});
 
-  useEffect(() => {
-    if (!isLoading) {
-      const stripePaymentLink = localStorage.getItem("stripePaymentLink");
-      if (data?.success && stripePaymentLink && user?.email) {
-        localStorage.removeItem("stripePaymentLink");
-        router.push(stripePaymentLink + `?checkout[email]=${user.email}`);
-      } else if (data?.success === false) {
-        router.push("/");
-      }
-    }
-  }, [isLoading, router, user, data]);
+	useEffect(() => {
+		const stripePaymentLink = localStorage.getItem("stripePaymentLink");
+		if (data?.success && stripePaymentLink && user?.email) {
+			localStorage.removeItem("stripePaymentLink");
+			router.push(stripePaymentLink + `?checkout[email]=${user.email}`);
+		} else if (data?.success === false) {
+			router.push("/");
+		}
+	}, [router, user, data]);
 
-  if (data?.success) router.push("/home");
-  if (isLoading) {
-    return (
-      <div className="mt-20 w-full flex justify-center">
-        <div className="flex flex-col items-center gap-2">
-          <Loader className="w-10 h-10 animate-spin text-primary" />
-          <h3 className="text-xl font-bold">Redirecting...</h3>
-          <p>Please wait...</p>
-        </div>
-      </div>
-    );
-  }
+	if (data?.success) router.push("/home");
 
-  return null;
+	return (
+		<div className='mt-20 w-full flex justify-center'>
+			<div className='flex flex-col items-center gap-2'>
+				<Loader className='w-10 h-10 animate-spin text-primary' />
+				<h3 className='text-xl font-bold'>Redirecting...</h3>
+				<p>Please wait...</p>
+			</div>
+		</div>
+	);
 };
 export default Page;
