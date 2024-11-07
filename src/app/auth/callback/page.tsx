ccd9f8ca7b50e8bbe69bb7c46ea8c0f17abc8178
ayans <ayans@synopsys.com>
2024-11-07 18:16:20 +0530

    Removed auth0

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -4,47 +4,45 @@ import { Loader } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { checkAuthStatus } from "./actions";
 import { useEffect } from "react";
-import { useUser } from "@auth0/nextjs-auth0/client";
 
 const Page = () => {
-  const router = useRouter();
-  const { user, error } = useUser();
-  const { data, isLoading } = useQuery({
-    queryKey: ["checkAuthStatus"],
-    queryFn: async () => await checkAuthStatus(),
-    staleTime: Infinity, // Optional: prevent frequent refetches
-  });
+  // const router = useRouter();
+  // const { data, isLoading } = useQuery({
+  //   queryKey: ["checkAuthStatus"],
+  //   queryFn: async () => await checkAuthStatus(),
+  //   staleTime: Infinity, // Optional: prevent frequent refetches
+  // });
 
-  console.log("qwertyuio....................")
-  useEffect(() => {
-    if (!isLoading) {
-      const stripePaymentLink =
-        typeof window !== "undefined"
-          ? localStorage.getItem("stripePaymentLink")
-          : null;
+  // console.log("qwertyuio....................")
+  // useEffect(() => {
+  //   if (!isLoading) {
+  //     const stripePaymentLink =
+  //       typeof window !== "undefined"
+  //         ? localStorage.getItem("stripePaymentLink")
+  //         : null;
 
-      if (data?.success && stripePaymentLink && user?.email) {
-        localStorage.removeItem("stripePaymentLink");
-        router.push(stripePaymentLink + `?checkout[email]=${user.email}`);
-      } else if (data?.success === false) {
-        router.push("/");
-      } else if (data?.success) {
-        router.push("/home");
-      }
-    }
-  }, [isLoading, data, router, user]);
+  //     if (data?.success && stripePaymentLink && user?.email) {
+  //       localStorage.removeItem("stripePaymentLink");
+  //       router.push(stripePaymentLink + `?checkout[email]=${user.email}`);
+  //     } else if (data?.success === false) {
+  //       router.push("/");
+  //     } else if (data?.success) {
+  //       router.push("/home");
+  //     }
+  //   }
+  // }, [isLoading, data, router, user]);
 
-  if (isLoading) {
-    return (
-      <div className="mt-20 w-full flex justify-center">
-        <div className="flex flex-col items-center gap-2">
-          <Loader className="w-10 h-10 animate-spin text-primary" />
-          <h3 className="text-xl font-bold">Redirecting...</h3>
-          <p>Please wait...</p>
-        </div>
-      </div>
-    );
-  }
+  // if (isLoading) {
+  //   return (
+  //     <div className="mt-20 w-full flex justify-center">
+  //       <div className="flex flex-col items-center gap-2">
+  //         <Loader className="w-10 h-10 animate-spin text-primary" />
+  //         <h3 className="text-xl font-bold">Redirecting...</h3>
+  //         <p>Please wait...</p>
+  //       </div>
+  //     </div>
+  //   );
+  // }
 
   return null;
 };
